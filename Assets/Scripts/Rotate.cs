6e8fc9beb056c826c839f5c5d7e0c95933b33c1a
nun5522 <nuntawat.sorn@bumail.net>
2024-03-19 09:40:47 +0700

    Rotate_Part1 Finish

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 torque,angularV;
+
+    private void RotateTorque()
+    {
+        rb.AddTorque(torque);
+    }
+
+    private void RotateAngularV()
+    {
+        rb.angularVelocity = angularV;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RotateAngularV();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RotateTorque();
+        }
+    }
+}
